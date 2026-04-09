e4f97b01ab274ef9a53cbba0962d0321398aa450
Sarah Abowitz <mazemaker@icloud.com>
2026-04-09 15:28:23 +0000

    initial pilot preparations

---
+++ claimful_switcher.js
@@ -0,0 +1,25 @@
+const notebook = document.getElementById("notebook");
+const source_text = document.getElementById("source_text");
+notebook.style.display = "none";
+
+const source = document.getElementById("source");
+console.log(source.style.height);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const analysis_toggle = document.getElementById('analysis_toggle');
+  // make toggles a class, get all toggles, give them bespoke listener
+
+  analysis_toggle.addEventListener('click', function() {
+    if (this.innerHTML.indexOf("Show") > -1) {
+      this.innerHTML = "Hide Claim Analysis";
+      source_text.style.display = "none";
+      notebook.style.display = "block";
+    } else {
+      this.innerHTML = "Show Claim Analysis";
+      notebook.style.display = "none";
+      source_text.style.display = "block";
+    }
+  }
+  );
+
+});
